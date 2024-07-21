b4a3431cce207d3e66ff459b74f738209e732639
TeeeCodes <Terencepriesterr@gmail.com>
2024-07-20 23:57:36 -0400

    changes tracked

--- mybackend/cardapp/urls.py
+++ mybackend/cardapp/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from .views import CardList
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', CardList.as_view(), name='card-list'),
 ]
 
